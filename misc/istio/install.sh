6e1f1425028dd28d6cc59919e1102736e5de3785
Yany Choi <dydghks0818@naver.com>
2023-11-24 14:43:00 +0900

    add istio script

--- misc/istio/install.sh
+++ misc/istio/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-kubectl config use-context ops
-istioctl install --context=ops -f ops.yaml
-kubectl config use-context prod
-istioctl install --context=prod -f prod.yaml
+istioctl install --context=ops --set profile=demo -f ops.yaml
+istioctl install --context=prod --set profile=demo -f prod.yaml
 
 istioctl create-remote-secret --context=ops --name=ops-cluster | kubectl apply -f - --context=prod
 istioctl create-remote-secret --context=prod --name=prod-cluster | kubectl apply -f - --context=ops
